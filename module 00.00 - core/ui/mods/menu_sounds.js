74691a6a4624784241977c146c420dcdaa1bb2c2
thos-grol <andrewdo789@proton.me>
2025-11-29 01:23:02 -0800

    fixed bug with migration to modern hooks ie. back button wouldn't work

--- module 00.00 - core/ui/mods/menu_sounds.js
+++ module 00.00 - core/ui/mods/menu_sounds.js
@@ -24,21 +24,11 @@ var hk_btn_sounds =
 			SQ.call(hk_btn_sounds.mSQHandle, 'onHover')
 		})
 	},
-	registerMenuButtons : function()
-	{
-		$('[class*="button"]').each(function(){
-			if ($(this).data("SoundRegistered") !== true){
-				hk_btn_sounds.setCallbacks($(this));
-			}
-		})
-		Screens.MainMenuScreen.getModule("MainMenuModule").show = hk_btn_sounds.MainMenuScreen_show;
-	},
 	wrapper : function(_func)
 	{
 		return function()
 		{
 			var ret = _func.apply(this, arguments);
-			ret.data("SoundRegistered", true);
 			hk_btn_sounds.setCallbacks(ret);
 			return ret;	
 		}
@@ -49,12 +39,5 @@ $.fn.createTextButton = hk_btn_sounds.wrapper($.fn.createTextButton);
 $.fn.createCustomButton = hk_btn_sounds.wrapper($.fn.createCustomButton);
 $.fn.createTabTextButton = hk_btn_sounds.wrapper($.fn.createTabTextButton);
 $.fn.createImageButton = hk_btn_sounds.wrapper($.fn.createImageButton);
-
-// hk_btn_sounds.MainMenuScreen_show = Screens.MainMenuScreen.getModule("MainMenuModule").show;
-// Screens.MainMenuScreen.getModule("MainMenuModule").show = function(_animation)
-// {
-// 	hk_btn_sounds.registerMenuButtons();
-// 	return hk_btn_sounds.MainMenuScreen_show.call(this, _animation);
-// }
 
 registerScreen("hk_btn_sounds", hk_btn_sounds);	
